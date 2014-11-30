6bb3f15cb6870ef97ca3e9a579f351c5a9391287
Maximillian Laumeister <max.laumeister@gmail.com>
2014-11-30 15:25:57 -0800

    Pause when window loses visibility

--- app.js
+++ app.js
@@ -508,6 +508,35 @@ Automatune.init = function init(args) {
         alert("Reset function not yet implemented");
     };
     
+    // Pause when page is not visible
+    
+    // Set the name of the hidden property and the change event for visibility
+    var hidden, visibilityChange; 
+    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+      hidden = "hidden";
+      visibilityChange = "visibilitychange";
+    } else if (typeof document.mozHidden !== "undefined") {
+      hidden = "mozHidden";
+      visibilityChange = "mozvisibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      hidden = "msHidden";
+      visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      hidden = "webkitHidden";
+      visibilityChange = "webkitvisibilitychange";
+    }
+    
+    function handleVisibilityChange() {
+      if (document[hidden]) {
+        Automatune.pause();
+        Howler.mute();
+      } else {
+        Howler.unmute();
+      }
+    }
+    
+    document.addEventListener(visibilityChange, handleVisibilityChange, false);
+    
     // Add a visitor
     visitors.push(new gridVisitor(3, 3, O_RIGHT));
     
