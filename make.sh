0a61d93b5e1de3eaa7f07c58b3d2ee70da678d4e
Maximillian Laumeister <max.laumeister@gmail.com>
2014-12-19 21:14:20 -0800

    Add minification workflow

---
+++ make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This command is for Google's Closure Compiler
+
+java -jar compiler.jar --language_in=ECMASCRIPT5 --js_output_file dist/automatune.min.js --js lib/jquery-ui.js --js lib/jquery.ui-contextmenu.js --js lib/jquery.ui.message.js --js lib/howler.js --js lib/web-animations.min.js --js lib/ohsnap.js --js src/automatune.js
+
+#
