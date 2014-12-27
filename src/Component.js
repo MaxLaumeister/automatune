c4269f23a11d28f3268406ed64bc5ad980bea73f
Maximillian Laumeister <max.laumeister@gmail.com>
2014-12-26 23:10:19 -0800

    Rewrite progress

---
+++ src/Component.js
@@ -0,0 +1,8 @@
+/**
+ * A component that goes on a cell, such as an arrow or note.
+ * @constructor
+ */
+Automatune.Component = function() {
+    /** The type of component (arrow, note, etc.) */
+    var type;
+}
---
+++ src/GridCell.js
@@ -0,0 +1,24 @@
+/**
+ * A cell on the game grid.
+ * @constructor
+ */
+Automatune.GridCell = function() {
+    
+    /** The DOM Element associated with this GridCell */
+    var domElement;
+    /** The Components associated with this GridCell */
+    var components;
+    
+    /** Append a component to this GridCell. */
+    this.append = function(ct) {
+        
+    }
+    /** Remove a component from this GridCell. */
+    this.remove = function(ct) {
+        
+    }
+    /** Called when this GridCell is visited by a Visitor. */
+    this.onVisit = function() {
+    
+    }
+}
