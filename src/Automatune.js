4ebf187119597d891916375ac469c73c18050d33
Maximillian Laumeister <max.laumeister@gmail.com>
2014-12-27 16:49:43 -0800

    Rewrite progress

--- src/Automatune.js
+++ src/Automatune.js
@@ -4,7 +4,9 @@
  */
 function Automatune() {
     
-    /** Returns the GridCell at position {x, y}. 
+    /**
+     * Returns the GridCell at position {x, y}. 
+     * @public
      * @param {int} x The x position of the GridCell.
      * @param {int} y The y position of the GridCell.
      * @returns {GridCell}
@@ -13,10 +15,16 @@ function Automatune() {
         return gridCells[x][y];
     }
     
-    /** A 2D list of all grid cells. **/
+    /**
+     * A 2D list of all grid cells.
+     * @private
+     */
     var gridCells = [[]];
     
-    /** Objects to call update() on every step. */
+    /**
+     * Objects to call update() on every step.
+     * @private
+     */
     var updateTargets = [];
     
 }
